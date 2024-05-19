3d9cebbc4495efa35308fb95204a394db83d8149
githubLGelber <45414438+githubLGelber@users.noreply.github.com>
2024-05-19 22:42:10 +1200

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,21 @@
 import pandas as pd
 import streamlit as st
 
-#DATA=pd.read_csv("../64226/TB_Burden_Country.csv")
 
 dir_name = os.path.abspath(os.path.dirname('TB_Burden_Country.csv'))
 DATA = os.path.join(dir_name, 'TB_Burden_Country.csv')
-st.dataframe(DATA)
+DATA2=pd.read_csv(DATA)
+
+
+
+
+st.dataframe(DATA2)
+
+
+
+
+
+
 st.title('My First Streamlit App')
 
 user_input = st.text_input("Enter some text")
