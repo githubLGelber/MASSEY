52542c3d235bf7623d59fadb5a9deb9d1c6807f7
githubLGelber <45414438+githubLGelber@users.noreply.github.com>
2024-05-19 21:49:19 +1200

    Add files via upload

---
+++ example2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import streamlit as st
+
+#DATA=pd.read_csv("../64226/TB_Burden_Country.csv")
+#st.dataframe(DATA)
+
+st.title('My First Streamlit App')
+
+user_input = st.text_input("Enter some text")
+st.write('The user entered:', user_input)
